635b0c59f84936256cb93dcdd8549399b5f5a4f3
Krista Goralczyk <krista.goralczyk@gmail.com>
2019-12-02 21:55:51 -0500

    nwb for real

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { render } from 'react-dom';
+// import React from 'react';
+// import { render } from 'react-dom';
+// import App from './App';
+// render(<App />, document.querySelector('#app'));
 
-import App from './App';
-
-render(<App />, document.querySelector('#app'));
+export { default as CozyHoliday } from './App';
